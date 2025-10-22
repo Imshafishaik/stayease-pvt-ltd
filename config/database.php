c8d00b4e2474a55da93bea65310d2cea78b6bc12
Shafi Shaik <shafi@Shafis-MacBook-Air.local>
2025-10-22 23:20:14 +0200

    database connection part done

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$port = "5432";
+$dbname = "crud_db";
+$user = "shafi";
+$pass = "shafi"; 
+
+try {
+    //pdo - php data object
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "✅ Connected to PostgreSQL successfully!";
+} catch (PDOException $e) {
+    die("PostgreSQL connection failed: " . $e->getMessage());
+}
+?>
