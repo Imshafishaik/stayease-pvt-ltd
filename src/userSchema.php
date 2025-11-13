f461a038a76ac0c5519d22e039dc4be8d5073f1a
Shafi Shaik <shafi@Shafis-MacBook-Air.local>
2025-11-13 13:42:44 +0100

    small changes

--- src/userSchema.php
+++ src/userSchema.php
@@ -1,10 +1,30 @@
 <?php
 require __DIR__ . '/../config/database.php';
 
-$sql = "CREATE TABLE shafi(shafiid SERIAL PRIMARY KEY, shafiname VARCHAR(100) NOT NULL, shafiemail VARCHAR(100) UNIQUE NOT NULL)";
+try {
+// $sql = "CREATE TABLE shafi(shafiid SERIAL PRIMARY KEY, shafiname VARCHAR(100) NOT NULL, shafiemail VARCHAR(100) UNIQUE NOT NULL)";
+$stmt = $pdo->prepare("INSERT INTO shafi (user_name) VALUES (:name)");
 
-$pdo->exec($sql);
+$stmt->execute([
+    ':name' => "rahul",
+]);
 
-echo "Table created successfully..."
+// $stmt = $pdo->query("SELECT shafiid, shafiname, shafiemail FROM shafi");
+// while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//     echo "ID: {$row['shafiid']} - Name: {$row['shafiname']} - Email: {$row['shafiemail']} <br>";
+// }
+
+$stmt = $pdo->query("SELECT * FROM users");
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $stmt = $pdo->query("SELECT shafiid, shafiname, shafiemail FROM shafi");
+// $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// $pdo->exec($sql);
+// echo $results;
+print_r($results);
+echo "Table created successfully...";
+} catch (PDOException $e) {
+    echo "❌ Error: " . $e->getMessage();
+}
 
 ?>
