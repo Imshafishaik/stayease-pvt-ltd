904a4782cf3ded0f92516423370ac1b363fd9fa2
Shafi Shaik <shafi@Shafis-MacBook-Air.local>
2025-11-26 14:18:35 +0100

    small chnages

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+
+$host = "shafi-shafimern-c5d9.h.aivencloud.com";
+$port = "19448";
+$dbname = "defaultdb";
+$user = "avnadmin";
+$pass = $_ENV['DB_PASS'] ?? null;
+
+try {
+    //pdo - php data object
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    // echo "✅ Connected to PostgreSQL successfully!";
+} catch (PDOException $e) {
+    die("PostgreSQL connection failed: " . $e->getMessage());
+}
+?>
